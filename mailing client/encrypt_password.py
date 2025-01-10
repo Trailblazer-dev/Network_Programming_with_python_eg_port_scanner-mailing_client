47bcef852591b36b4ffe95199df0bbd791fabec4
Trailblazer-dev <richvictor830@gmail.com>
2025-01-10 20:42:47 +0300

    commit

---
+++ mailing client/encrypt_password.py
@@ -0,0 +1,18 @@
+from cryptography.fernet import Fernet
+
+# Generate a key and write it to a file
+key = Fernet.generate_key()
+with open('secret.key', 'wb') as key_file:
+    key_file.write(key)
+
+# Encrypt the password
+password = '4565'.encode()
+cipher_suite = Fernet(key)
+cipher_text = cipher_suite.encrypt(password)
+
+# Write the encrypted password to a file
+with open('encrypted_password.bin', 'wb') as encrypted_file:
+    encrypted_file.write(cipher_text)
+    
+    
+# print("Key and encrypted password files have been generated.")
